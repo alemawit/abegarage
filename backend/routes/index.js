537a8aea43e1396db531e99607b5da7ff9008ec9
alemawit <alemawit.woldu@gmail.com>
2025-01-21 23:26:29 -0500

    install route controller and service added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,11 @@
 //import all routes
+//import the express module
 const express = require('express');
+//call the router method from the express module to create a new router
 const router = express.Router();
+//import the install router
+const installRouter = require('./install.routes');
+//add the install router to the main routes
+router.use(installRouter);
+//export the router
+module.exports = router;
